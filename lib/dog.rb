ade96c80c9ade897c0e316209d3a5d0edb858355
Eugene White <eugenewhite1994@gmail.com>
2020-07-21 00:53:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,4 +8,16 @@ class Dog
   def name
     @this_dogs_name
   end
+  
+  def bark=(bark_bark)
+    @dogs_bark = bark_bark
 end
+
+def bark 
+  @dogs_bark
+end
+
+fido = Dog.new
+fido.name = "Fido"
+
+fido.bark
