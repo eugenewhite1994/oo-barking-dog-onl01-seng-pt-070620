47f51a6185e620ffdf5d7c3cadf84ba58bc1f4f8
Eugene White <eugenewhite1994@gmail.com>
2020-07-21 00:54:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,13 @@
-# Your code goes here!
 class Dog
- 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
+  def name= (name)
+    @name = name
   end
- 
   def name
-    @this_dogs_name
+    @name
   end
-  
-  def bark=(bark_bark)
-    @dogs_bark = bark_bark
+  def bark
+    puts "woof!"
+  end
+
+
 end
-
-def bark 
-  @dogs_bark
-end
-
-fido = Dog.new
-fido.name = "Fido"
-
-fido.bark
